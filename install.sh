5a1cff908b690b32e799ae0e6f08270a0b116f77
france97 <francesco.ferraguti@gmail.com>
2022-06-22 15:30:08 +0200

    Update scripts

--- install.sh
+++ install.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
+configure() {
+  touch config.yaml
+
+  answer="n"
+  while [ $answer != 'y' ]; do
+    echo "Insert your username: "
+    read -r username
+
+    echo "Is this your username $username? (y/n) "
+    read -r answer
+  done
+
+  answer="n"
+  while [ $answer != 'y' ]; do
+    echo "Insert your password: "
+    read -r password
+
+    echo "Is this your username $password? (y/n) "
+    read -r answer
+  done
+
+  printf "APP:\n\tUSERNAME: %s\n\tPASSWORD: %s" "$username" "$password" >> config.yaml
+}
+
 python -m pip install --user --upgrade pip
 python -m venv venv
 
-touch config.yaml
-
-answer="n"
-while [ $answer != 'y' ]; do
-  echo "Insert your username: "
-  read -r username
-
-  echo "Is this your username $username? (y/n) "
-  read -r answer
-done
-
-answer="n"
-while [ $answer != 'y' ]; do
-  echo "Insert your password: "
-  read -r password
-
-  echo "Is this your username $password? (y/n) "
-  read -r answer
-done
-
-printf "APP:\n\tUSERNAME: %s\n\tPASSWORD: %s" "$username" "$password" >> config.yaml
+if [ ! -f config.yaml ]; then
+  configure
+fi
 
 [[ $(uname -a | grep ndroid) ]] && termux-fix-shebang run.sh
